d36761b94292a603bc963672e3fa5904471f8d78
lkyi <lkyi@senthink.com>
2022-01-04 17:54:52 +0800

    init

--- simple-hmily/consumer/src/main/java/com/lkyi/RemoteController.java
+++ simple-hmily/consumer/src/main/java/com/lkyi/RemoteController.java
@@ -4,18 +4,24 @@ import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.dromara.hmily.annotation.HmilyTCC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@DefaultProperties(defaultFallback = "fallBack")
+//@DefaultProperties(defaultFallback = "fallBack")
 public class RemoteController {
 
     @DubboReference
     private HelloDubboService helloDubboService;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 //    @HystrixCommand(fallbackMethod = "fallBack", commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")})
 //    @HystrixCommand(commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")})
+    @HmilyTCC(confirmMethod = "confirm", cancelMethod = "cancel")
     @GetMapping("/helloDubbo")
     public String helloDubbo(String msg) throws InterruptedException {
         return helloDubboService.hello("from consumer");
@@ -24,5 +30,16 @@ public class RemoteController {
     public String fallBack() {
         return "fallback";
     }
+
+    public String confirm() {
+        logger.info("confirm");
+        return "confirm";
+    }
+
+    public String cancel() {
+        logger.info("cancel");
+        return "cancel";
+    }
+
 
 }
--- simple-hmily/producer/src/main/java/com/lkyi/HelloDubboServiceImpl.java
+++ simple-hmily/producer/src/main/java/com/lkyi/HelloDubboServiceImpl.java
@@ -1,18 +1,30 @@
 package com.lkyi;
 
 import org.apache.dubbo.config.annotation.DubboService;
+import org.dromara.hmily.annotation.HmilyTCC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@DubboService()
+@DubboService
 public class HelloDubboServiceImpl implements HelloDubboService{
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
+    @HmilyTCC(confirmMethod = "confirm", cancelMethod = "cancel")
     public String hello(String msg) throws InterruptedException {
-        Thread.sleep(5000);
+//        Thread.sleep(5000);
         logger.info("Accept msg: " + msg);
         return "Producer Accept msg " + msg;
+    }
+
+    public String confirm() {
+        logger.info("confirm");
+        return "confirm";
+    }
+
+    public String cancel() {
+        logger.info("cancel");
+        return "cancel";
     }
 }
