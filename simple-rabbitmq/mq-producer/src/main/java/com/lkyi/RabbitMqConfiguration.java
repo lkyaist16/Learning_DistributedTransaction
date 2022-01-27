66b5e15748bebf0ddc315f9f4fff6f983fd1ddbf
lkyi <lkyi@senthink.com>
2022-01-27 17:48:22 +0800

    init

--- simple-rabbitmq/mq-producer/src/main/java/com/lkyi/RabbitMqConfiguration.java
+++ simple-rabbitmq/mq-producer/src/main/java/com/lkyi/RabbitMqConfiguration.java
@@ -1,11 +1,33 @@
 package com.lkyi;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfiguration {
 
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("", 5672);
+        connectionFactory.setUsername();
+        connectionFactory.setPassword();
+        connectionFactory.setVirtualHost();
+        connectionFactory.setPublisherConfirmType(CachingConnectionFactory.ConfirmType.CORRELATED);
+        return connectionFactory;
+    }
 
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setMandatory(true);
+        rabbitTemplate.setConfirmCallback(() ->{
+            if() {
+
+            }
+
+            String msgId =
+        });
+    }
 
 
 }
